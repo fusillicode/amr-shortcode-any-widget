b732cb9c206413f03ace4b107c5e861a5f78d76a
anmari <anmari@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-25 10:40:38 +0000

    git-svn-id: https://plugins.svn.wordpress.org/amr-shortcode-any-widget/trunk@221592 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- amr_shortcode_any_widget.php
+++ amr_shortcode_any_widget.php
@@ -4,7 +4,7 @@ Plugin Name: amr shortcode any widget
 Plugin URI: http://webdesign.anmari.com/shortcode-any-widget/
 Description: Allows inclusion of any widget within a page for any theme.  [do_widget widgetname ] or  [do_widget "widget name" ]
 Author: Anna-marie Redpath
-Version: 1.0 alpha
+Version: 1.2
 Author URI: http://webdesign.anmari.com
 
 */
@@ -13,31 +13,57 @@ Author URI: http://webdesign.anmari.com
 /*-----------------------------------*/
 function do_widget($atts) {
 
-global $wp_registered_widgets,$_wp_sidebars_widgets;
+global $wp_registered_widgets, $_wp_sidebars_widgets,$wp_registered_sidebars;
 
 /* check if the widget is in  the shortcode x sidebar  if not , just use generic, 
 if it is in, then get the instance  data and use that */
-if (isset($_REQUEST['debug'])) $debug=true;
+if (isset($_REQUEST['do_widget_debug'])) $debug=true;
+
+	if (isset($_wp_sidebars_widgets) ) {
+		if ($debug) { 
+		var_dump($atts);
+			echo '<br />Available sidebars and widgets<br />';
+			foreach ($_wp_sidebars_widgets as $i=> $w) {
+				echo 'Sidebar:&nbsp;<b>'.$i.': '.get_sidebar_name($i).'</b><br />';
+				if (is_array($w)) {
+					sort ($w);
+					foreach ($w as $i2=> $w2) {
+					echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$w2.' <br />';
+					};
+				}
+				echo '<br />';
+			};
+		}
+	}
+	else { if ($debug) {echo '<br />No widgets defined'; }
+			return (false);
+		}
 
 	extract(shortcode_atts(array(
-		'sidebar' => 'Shortcode',
+		'sidebar' => 'Shortcodes',
 		'id' => '',
 		'title' => 'true'   /* do the default title unless they ask us not to - use string here not boolean */
 	), $atts));
+	
+
 
 	/* the widget need not be specified, [do_widget widgetname] is adequate */
 	if (!empty($atts[0])) {
+		if ($debug) echo 'We have a name';
 		$widget = $atts[0];
+		
 		foreach ($wp_registered_widgets as $i => $w) { /* get the official internal name or id that the widget was registered with  */
-			if ($w['name'] === $widget) $widget_ids[] = $i;
+			if (strtolower($w['name']) === strtolower($widget)) $widget_ids[] = $i;
 		}
+		
 	}	
-	else { /* check for id */
+	else { /* check for id if we do not have a name */
 			if (!empty($id))  { 	/* if a specific id has been specified */
+				
 				foreach ($wp_registered_widgets as $i => $w) { /* get the official internal name or id that the widget was registered with  */
-				if ($debug) {	echo '<br> id = '.$id.' - '.$w['id'] ;}
-					if ($w['id'] === $id) $widget_ids[] = $i;
+					if ($w['id'] === $id) $widget_ids[] = $id;
 				}
+				if ($debug) {echo '<h2>We have an id: '.$id.'</h2>'; var_dump($widget_ids);}
 			}
 			else {
 				if ($debug) {	echo 'No valid widget name or id given';}			
@@ -45,38 +71,51 @@ if (isset($_REQUEST['debug'])) $debug=true;
 				
 			}
 	}
+	
 	if (empty ($widget_ids)) { 
-		if ($debug) {	echo '<h2>Widget not found in widget list </h2>'; print_r($wp_registered_widgets);}
-		return ;
-		}
-	if ($title=='false') $title = false;
+		if ($debug) {	echo '<b>Widget not found in widget list </b>'; }
+		return (false) ;
+	}
+
+	if ($title == 'false') $title = false; /* If ask not to display title, then do not */
 	else $title = true;
 	
-	$sidebarid = get_sidebar_id ($sidebar);   /* get the official sidebar id - will take the first one */
+	if (!($sidebarid = get_sidebar_id ($sidebar))) $sidebarid=$sidebar;   /* get the official sidebar id - will take the first one */
 	
-	if ($debug) {	echo '<hr><h2>Looking for widget:'.$widget.' </h2>';		echo 'found instances:'; print_r ($widget_ids);		}
+	if ($debug) {	
+		echo '<hr>Looking for widget with name:'.$widget.' or id='.$id.' Found instances:'.' <br />'; 
+		foreach ($widget_ids as $i=> $w) {
+			echo $w.'<br />';
+		};		
+	}
 	$content = ''; 			
 	/* if the widget is in our chosen sidebar, then use the otions stored for that */
-	if (isset($_wp_sidebars_widgets) ) {
-		if ((isset ($_wp_sidebars_widgets[$sidebarid])) and (!empty ($_wp_sidebars_widgets[$sidebarid]))) {
-			if ($debug) { 
-				echo '<h2>Widget ids in sidebar in sidebar '.$sidebar.' with id:'.$sidebarid .'</h2>';
-				print_r($_wp_sidebars_widgets[$sidebarid]);
-			}
+
+	if ((isset ($_wp_sidebars_widgets[$sidebarid])) and (!empty ($_wp_sidebars_widgets[$sidebarid]))) {
+		if ($debug) { 
+			echo '<br />Widget ids in sidebar: "'.$sidebar.'" with id: '.$sidebarid .'<br />';
+			sort ($_wp_sidebars_widgets[$sidebarid]);
+			foreach ($_wp_sidebars_widgets[$sidebarid] as $i=> $w) {
+				echo $i.' '.$w.'<br />';
+			};	
+		}
 			/* get the intersect of the 2 widget setups so we just get the widget we want  */
+
 			$wid = array_intersect ($_wp_sidebars_widgets[$sidebarid], $widget_ids );
-			if ($debug) { echo '<br />Chosen Widget ids in Chosen sidebar';print_r($wid);}
+			if ($debug) { echo '<br />Will use widget ids'.'<br />';
+				foreach ($widget_ids as $i=> $w) {
+					echo '&nbsp;&nbsp;&nbsp;'.$w.'<br />';
+				};
+			}
 		}
 		else { /* the sidebar is not defined */
-			if ($debug) {echo '<br />Sidebar '.$sidebar.' empty or not defined.'; }
+			if ($debug) {echo '<br />Sidebar '.$sidebar.'with sidebarid '.$sidebarid.' empty or not defined.'; }
 		}
-	}
-	else { if ($debug) {echo '<br />No widgets defined'; }
-			return (false);
-		}
+	
+
 	if (empty ($wid)) { 
-		unset($sidebar); unset($sidebarid);
 		if ($debug) {	echo '<h2>No Widget ids in sidebar '.$sidebarid.' with name '.$sidebar.' Try defaults </h2>';}
+		unset($sidebar); unset($sidebarid);
 		}
 	else 	
 		/*  There may only be one but if we have two in our chosen widget then it will do both */
@@ -87,7 +126,18 @@ if (isset($_REQUEST['debug'])) $debug=true;
 			}
 	return ($output);
 }
-
+/*-----------------------------------*/
+function get_sidebar_name ($id) { /* dont need anymore ? or at least temporarily */
+/* walk through the registered sidebars with a name and find the id - will be something like sidebar-integer.  take the first one */
+global $wp_registered_sidebars;	
+	foreach ($wp_registered_sidebars as $i => $a) {
+		if ((isset ($a['id'])) and ( $a['id'] === $id)) {
+			if (isset($a['name'])) return ($a['name']);
+			else return ($id);
+		}
+	}
+	return (false);
+}
 /*-----------------------------------*/
 function get_sidebar_id ($name) { /* dont need anymore ? or at least temporarily */
 /* walk through the registered sidebars with a name and find the id - will be something like sidebar-integer.  take the first one */
@@ -113,16 +163,24 @@ function shortcode_sidebar( $id, $index=1, $title=true) { /* This is basically t
 		}
 	}
 
-	$sidebars_widgets = wp_get_sidebars_widgets();
-	/* if there are no active widgets */
-	if ( empty($wp_registered_sidebars[$index]) || 
-		!array_key_exists($index, $sidebars_widgets) || 
-		!is_array($sidebars_widgets[$index]) 
-		|| empty($sidebars_widgets[$index]) )
-		return false;
+	$sidebars_widgets = wp_get_sidebars_widgets(); 
+	
+	if ($debug) foreach ($wp_registered_widgets as $i=>$w) {
+		echo '<br />'.$w['name'].' '.$w['id'];
+	};
+	
+	
+	/* DONT NEED TO BE ACTIVE ? if there are no active widgets */
+//	if ( empty($wp_registered_sidebars[$index]) || 
+//		!array_key_exists($index, $sidebars_widgets) || 
+//		!is_array($sidebars_widgets[$index]) 
+//		|| empty($sidebars_widgets[$index]) ) {
+//				echo '<br />'.'No active widgets for '.$index;
+//				return false;
+//	}
 
-	$sidebar = $wp_registered_sidebars[$index];
-//	$sidebar = array('wp_inactive_widgets');
+//	$sidebar = $wp_registered_sidebars[$index];
+	$sidebar = array('wp_inactive_widgets');
 	$did_one = false;
 	 
 //	foreach ( (array) $sidebars_widgets[$index] as $id ) {    /* lifted from wordpress code, keep as similar as possible for now */
@@ -161,15 +219,22 @@ function shortcode_sidebar( $id, $index=1, $title=true) { /* This is basically t
 	return $did_one;
 }
 
-/* -------------------------------------------------------------------------------------------------------------*/
-add_shortcode('do_widget', 'do_widget');
-/* Create a sidebar that will not appear in any theme, but can be used to customise widget settings if default settings not suitable */
+function amr_reg_sidebar() {
 if ( function_exists('register_sidebar') )  
-	register_sidebar(array('name'=>'Shortcode',
-		'id'            => 'sidebar-shortcode',
+	register_sidebar(array('name'=>'Shortcodes',
+		'id'            => 'Shortcodes',
 		'before_widget' => '<div id="%1$s" class="widget %2$s clearfix">',
 		'after_widget'  => '</div>',
 		'before_title'  => '<h2 class="widgettitle">',
 		'after_title'   => '</h2>' )); 
+}
+/* -------------------------------------------------------------------------------------------------------------*/
+
+add_action('admin_init', 'amr_reg_sidebar'); 
+add_shortcode('do_widget', 'do_widget');
+
+// add_action('setup_theme', 'do_widget_sidebar', 99);  /*  Try this so the register sidebar will execute later after theme setup.  Unnamed sidebars were being impacted.  ** Does not help - wp seems to lose the widget setup then - does not keep it.*/
+//function do_widget_sidebar () {
+/* Create a sidebar that will not appear in any theme, but can be used to customise widget settings if default settings not suitable */
 
 ?>
--- readme.txt
+++ readme.txt
@@ -2,19 +2,28 @@
 Contributors: Anmari
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=anmari%40anmari%2ecom&item_name=Shortcode widget Plugin Support&no_shipping=1&no_note=1&cn=Optional%20Notes&tax=0&currency_code=USD&bn=PP%2dDonationsBF&charset=UTF%2d8">Donate</a>
 Tags: shortcode widget page
-Tested up to: 2.8.4
-Version: 1.0 alpha
-Stable tag: trunk
+Tested up to: 2.9.2
+Version: 1.2
+Stable tag: 1.2
 
 == Description ==
 This simple 'utility' plugin allows one to have any widget used in a page shortcode in any theme - no need to use the hybrid theme or create a special template.  
-
+[Support]
 If you found the plugin useful, please <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=anmari%40anmari%2ecom&item_name=Shortcode widget Support&no_shipping=1&no_note=1&cn=Optional%20Notes&tax=0&currency_code=USD&bn=PP%2dDonationsBF&charset=UTF%2d8">Donate</a>
 
 == Changelog ==
-= 1.0 =
-* Launch of the plugin
+= 1.12=
 
+*  Changed dummy shortcode sidebar so it appears after the theme sidebars to avoid taking over their widgets (this happened in numbered sidebars)
+*  Tested in 2.9.2, can still use either the widget name "Categories" or id=categories-6.  Note widget must have been created in the dummy shortcode sidebar.
+
+= 1.1 =
+
+*  Allow for lowercase use of widget names for the those challenged by attention to detail!
+*  Cleaned up debug mode to make it easier for people to identify which instance of a widget they are using.  Add &do_widget_debug to url string.
+
+= 1.0 =
+*  Launch of the plugin
 
 == Installation ==
 
@@ -22,10 +31,10 @@ If you found the plugin useful, please <a href="https://www.paypal.com/cgi-bin/w
 2. Activate the plugin through the 'Plugins' menu in WordPress
 3. Activate the widget plugin that you want to use.  
 4. Go to the widgets menu and drag an instance of the widget to the shortcodes sidebar, and set the widget options.
-5. Create or edit the page or post in which you wish to use the widget, enter [do_widget widgetname] within the text in the page and save.  
+5. Create or edit the page or post in which you wish to use the widget, enter [do_widget widgetname] within the text in the page and save.  If the widget name has a space in it, use [do_widget "widget name"].
 
 Other variations:
-If you use a widget more than once for different shortcodes, you can use the widget id to isolate which widget instance (and of course assocaied settings to use).
+If you use a widget more than once for different shortcodes, you can use the widget id to isolate which widget instance (and of course associated settings to use).  ie: [do_widget id=categories-6]  
   
 
 
